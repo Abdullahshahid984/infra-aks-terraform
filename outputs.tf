2ed2ff36dc5708c793c39ac63e4ff703a554ddda
Abdullahshahid984 <135553582+Abdullahshahid984@users.noreply.github.com>
2025-08-25 03:50:19 +0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,15 @@ output "kube_config" {
   value     = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
+
+output "key_vault_id" {
+  value = azurerm_key_vault.kv.id
+}
+
+output "key_vault_name" {
+  value = azurerm_key_vault.kv.name
+}
+
+output "aks_principal_id" {
+  value = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+}
